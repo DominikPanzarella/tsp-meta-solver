e4ba2f5bd934a931ccddc526f1f0cfe39c5564bf
Dominik Panzarella <131593612+DominikPanzarella@users.noreply.github.com>
2025-06-23 11:54:58 +0200

    init version

---
+++ build-debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_TSP-META-SOLVER_FILE /Users/dominikpanzarella/Library/Mobile Documents/com~apple~CloudDocs/Desktop/SUPSI/Semestre 6/Tesi/tsp-meta-solver/build-debug/TSP-META-SOLVER.app/Contents/MacOS/TSP-META-SOLVER)
+set(__QT_DEPLOY_TARGET_TSP-META-SOLVER_TYPE EXECUTABLE)
