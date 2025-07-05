cfe0fdf5bfe5b66447008b7739e58b8a1dd18939
Dominik Panzarella <131593612+DominikPanzarella@users.noreply.github.com>
2025-07-05 16:18:47 +0200

    qt removed completely

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -1,11 +1,12 @@
-#include "mainwindow.h"
+#include <iostream>
 
-#include <QApplication>
+#include "controller/tspcontroller.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    const std::shared_ptr<TspController>& controller = TspController::getInstance();
+    
+    std::cout << "Funziona" << std::endl;
+
+    return 0;
 }
